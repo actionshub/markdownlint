8b6f33a56554ca96a6d7003240f648563f76bf2f
Sebastian Kunze <mail@sebastiankunze.de>
2020-02-17 16:49:50 +0100

    Fix output and preserve mdl exit code

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM ruby
-
 RUN gem install mdl
-
 COPY . .
-
 RUN chmod +x /entrypoint.sh
-
-ENTRYPOINT ["bash", "/entrypoint.sh"]
+ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
+#!/bin/bash
+
 output=$(mdl $INPUT_PATH)
-echo ::set-output name=output::"$output"
-if [ ! -z "$output" ]
-then
-    echo "FOUND MARKDOWN ISSUS"
-    exit 255
+exit_code=$?
+
+echo "$output"
+
+# See https://github.community/t5/GitHub-Actions/set-output-Truncates-Multiline-Strings/td-p/37870
+output="${output//'%'/'%25'}"
+output="${output//$'\n'/'%0A'}"
+output="${output//$'\r'/'%0D'}" 
+echo "::set-output name=output::$output"
+
+if [[ $exit_code != 0 ]]; then
+    exit $exit_code
 fi
