b3ce2e40bfc6d2561eb0fa7e716cea4e2d8ea16f
Jason Field <jason@avon-lea.co.uk>
2020-05-15 18:10:53 +0100

    Add problem matcher for mdl

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir ._actionshub_problem-matchers
+cp /markdownlint.json ._actionshub_problem-matchers/markdownlint.json
+echo "##[add-matcher]._actionshub_problem-matchers/markdownlint.json"
 output=$(mdl $INPUT_PATH)
 exit_code=$?
 
@@ -8,7 +11,7 @@ echo "$output"
 # See https://github.community/t5/GitHub-Actions/set-output-Truncates-Multiline-Strings/td-p/37870
 output="${output//'%'/'%25'}"
 output="${output//$'\n'/'%0A'}"
-output="${output//$'\r'/'%0D'}" 
+output="${output//$'\r'/'%0D'}"
 echo "::set-output name=output::$output"
 
 exit $exit_code
