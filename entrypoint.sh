213d04aa74b53a4a986f358b14a1a0eccdd32b9a
Dan Webb <dan.webb@damacus.io>
2023-02-09 02:05:22 +0000

    BREAKING CHANGE: Use a built Docker image (#28)
    - Build and push a Docker image rather than requiring the user built it
      every time
    - Move some of the entrypoint to correct COPY and ENV statements
    - Chmod and copy at the same time as we're now using buildx
    - Use Ruby slim
    - Use specific version numbers for Ruby & mdl so renovate will bump the
      version numbers
    
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-mkdir ._actionshub_problem-matchers
-cp /markdownlint.json ._actionshub_problem-matchers/markdownlint.json
-echo "##[add-matcher]._actionshub_problem-matchers/markdownlint.json"
-
 # in this context a blank string is an acceptable not set
 if [ -n "$INPUT_FILESTOIGNOREREGEX" ]; then
   # mdl does not currently support an exclude list so we use this to have that feature
@@ -20,6 +16,6 @@ echo "$output"
 output="${output//'%'/'%25'}"
 output="${output//$'\n'/'%0A'}"
 output="${output//$'\r'/'%0D'}"
-echo "output=$output" >> $GITHUB_OUTPUT
+echo "output=$output" >> "${GITHUB_OUTPUT}"
 
 exit $exit_code
